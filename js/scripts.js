2fdaef95ba325d9e90fe3079b14aa1d3eff302fe
Maheshwaran <maheshrajoffl@gmail.com>
2020-11-27 17:38:10 +0530

    Footer,login modal,signin modal and another article added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 const { ready } = require("jquery")
-
 $(document).ready(function() {
     $('[data-toggle = "tooltip"]').tooltip();
 });
+
+$(document).ready(function() {
+    $("#loginButton").click(function() {
+        $("#loginModal").modal('toggle');
+    });
+});
+
+                
+$(document).ready(function() {
+    $("#signinButton").click(function() {
+        $("#signinModal").modal('toggle');
+    });
+});
+
+
